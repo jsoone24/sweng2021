d6b43c3ed619cb0f9e55ba205eaf3dc074e73b23
Jongsoo <jsoone24@gmail.com>
2021-03-30 13:16:10 +0900

    base file reupload

--- pytet_v0.4/tetris.py
+++ pytet_v0.4/tetris.py
@@ -1,13 +1,11 @@
 from matrix import *
 from enum import Enum
 
-
 class TetrisState(Enum):
     Running = 0
     NewBlock = 1
     Finished = 2
-# end of class TetrisState():
-
+### end of class TetrisState():
 
 class Tetris():
     nBlockTypes = 0
@@ -17,23 +15,21 @@ class Tetris():
 
     @classmethod
     def init(cls, setOfBlockArrays):
-        Tetris.nBlockTypes = len(setOfBlockArrays)  # 7 블럭 개수
-        Tetris.nBlockDegrees = len(setOfBlockArrays[0])  # 4 블럭 각도 수
+        Tetris.nBlockTypes = len(setOfBlockArrays)
+        Tetris.nBlockDegrees = len(setOfBlockArrays[0])
         Tetris.setOfBlockObjects = [[0] * Tetris.nBlockDegrees for _ in range(Tetris.nBlockTypes)]
-        arrayBlk_maxSize = 0  # 모양중에 제일 큰거 기록
+        arrayBlk_maxSize = 0
         for i in range(Tetris.nBlockTypes):
             if arrayBlk_maxSize <= len(setOfBlockArrays[i][0]):
                 arrayBlk_maxSize = len(setOfBlockArrays[i][0])
-        # larget enough to cover the largest block
-        Tetris.iScreenDw = arrayBlk_maxSize
+        Tetris.iScreenDw = arrayBlk_maxSize     # larget enough to cover the largest block
 
         for i in range(Tetris.nBlockTypes):
             for j in range(Tetris.nBlockDegrees):
-                Tetris.setOfBlockObjects[i][j] = Matrix(
-                    setOfBlockArrays[i][j])  # 자신 클래스에 별도 저장
+                Tetris.setOfBlockObjects[i][j] = Matrix(setOfBlockArrays[i][j])
         return
 
-    def createArrayScreen(self):  # ArrayScreen(배경화면) 초기화
+    def createArrayScreen(self):
         self.arrayScreenDx = Tetris.iScreenDw * 2 + self.iScreenDx
         self.arrayScreenDy = self.iScreenDy + Tetris.iScreenDw
         self.arrayScreen = [[0] * self.arrayScreenDx for _ in range(self.arrayScreenDy)]
@@ -51,13 +47,13 @@ class Tetris():
 
         return self.arrayScreen
 
-    def __init__(self, iScreenDy, iScreenDx):  # 게임 화면 크기 설정
+    def __init__(self, iScreenDy, iScreenDx):
         self.iScreenDy = iScreenDy
         self.iScreenDx = iScreenDx
         self.idxBlockDegree = 0
         arrayScreen = self.createArrayScreen()
-        self.iScreen = Matrix(arrayScreen)  # ArrayScreen(배경) 만들어서 iScreen넣음
-        self.oScreen = Matrix(self.iScreen)  # iScreen으로 oScreen만듬
+        self.iScreen = Matrix(arrayScreen)
+        self.oScreen = Matrix(self.iScreen)
         self.justStarted = True
         return
 
@@ -67,62 +63,59 @@ class Tetris():
         if key >= '0' and key <= '6':
             if self.justStarted == False:
                 self.deleteFullLines()
-            self.iScreen = Matrix(self.oScreen)  # 현재 oScreen 받아옴
-            self.idxBlockType = int(key)  # 들어온 블럭 모양 저장
-            self.idxBlockDegree = 0  # 블럭 각도 설정
-            # 설정된 값으로 블럭 모양 읽어옴
+            self.iScreen = Matrix(self.oScreen)
+            self.idxBlockType = int(key)
+            self.idxBlockDegree = 0
             self.currBlk = Tetris.setOfBlockObjects[self.idxBlockType][self.idxBlockDegree]
             self.top = 0
-            self.left = Tetris.iScreenDw + self.iScreenDx//2 - self.currBlk.get_dx()//2  # 블럭 어디에 놓을지 위치 선정
+            self.left = Tetris.iScreenDw + self.iScreenDx//2 - self.currBlk.get_dx()//2
             self.tempBlk = self.iScreen.clip(self.top, self.left, self.top+self.currBlk.get_dy(), self.left+self.currBlk.get_dx())
-            # temp block 은 현재 화면에서 잘라온 블럭. currblock은 현재 선택된 블럭
             self.tempBlk = self.tempBlk + self.currBlk
             self.justStarted = False
             print()
 
-            if self.tempBlk.anyGreaterThan(1):  # 넘었는지 체크
+            if self.tempBlk.anyGreaterThan(1):
                 self.state = TetrisState.Finished
             self.oScreen = Matrix(self.iScreen)
-            self.oScreen.paste(self.tempBlk, self.top, self.left)  # 아니면 출력
+            self.oScreen.paste(self.tempBlk, self.top, self.left)
             return self.state
         elif key == 'q':
             pass
-        elif key == 'a':  # move left
+        elif key == 'a': # move left
             self.left -= 1
-        elif key == 'd':  # move right
+        elif key == 'd': # move right
             self.left += 1
-        elif key == 's':  # move down
+        elif key == 's': # move down
             self.top += 1
-        elif key == 'w':  # rotate the block clockwise
+        elif key == 'w': # rotate the block clockwise
             self.idxBlockDegree = (self.idxBlockDegree + 1) % Tetris.nBlockDegrees
             self.currBlk = Tetris.setOfBlockObjects[self.idxBlockType][self.idxBlockDegree]
-        elif key == ' ':  # drop the block
+        elif key == ' ': # drop the block
             while not self.tempBlk.anyGreaterThan(1):
-                self.top += 1
-                self.tempBlk = self.iScreen.clip(self.top, self.left, self.top+self.currBlk.get_dy(), self.left+self.currBlk.get_dx())
-                self.tempBlk = self.tempBlk + self.currBlk
+                    self.top += 1
+                    self.tempBlk = self.iScreen.clip(self.top, self.left, self.top+self.currBlk.get_dy(), self.left+self.currBlk.get_dx())
+                    self.tempBlk = self.tempBlk + self.currBlk
         else:
             print('Wrong key!!!')
-
+            
         self.tempBlk = self.iScreen.clip(self.top, self.left, self.top+self.currBlk.get_dy(), self.left+self.currBlk.get_dx())
         self.tempBlk = self.tempBlk + self.currBlk
 
-        if self.tempBlk.anyGreaterThan(1):  # 벽 충돌시 undo 수행
-            if key == 'a':  # undo: move right
+        if self.tempBlk.anyGreaterThan(1):   ## 벽 충돌시 undo 수행
+            if key == 'a': # undo: move right
                 self.left += 1
-            elif key == 'd':  # undo: move left
+            elif key == 'd': # undo: move left
                 self.left -= 1
-            elif key == 's':  # undo: move up
+            elif key == 's': # undo: move up
                 self.top -= 1
                 self.state = TetrisState.NewBlock
-            elif key == 'w':  # undo: rotate the block counter-clockwise
-                self.idxBlockDegree = (
-                    self.idxBlockDegree - 1) % Tetris.nBlockDegrees
+            elif key == 'w': # undo: rotate the block counter-clockwise
+                self.idxBlockDegree = (self.idxBlockDegree - 1) % Tetris.nBlockDegrees
                 self.currBlk = Tetris.setOfBlockObjects[self.idxBlockType][self.idxBlockDegree]
-            elif key == ' ':  # undo: move up
+            elif key == ' ': # undo: move up
                 self.top -= 1
                 self.state = TetrisState.NewBlock
-
+            
             self.tempBlk = self.iScreen.clip(self.top, self.left, self.top+self.currBlk.get_dy(), self.left+self.currBlk.get_dx())
             self.tempBlk = self.tempBlk + self.currBlk
 
@@ -134,4 +127,5 @@ class Tetris():
     def deleteFullLines(self):
         return
 
-# end of class Tetris():
+### end of class Tetris():
+    
